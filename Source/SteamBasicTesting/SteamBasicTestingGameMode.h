244a05917e0ff73573c011b4391465b4fd15d575
username <jjyoyo83@gmail.com>
2024-08-13 19:46:53 +0900

    initial commit

---
+++ Source/SteamBasicTesting/SteamBasicTestingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SteamBasicTestingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASteamBasicTestingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASteamBasicTestingGameMode();
+};
+
+
+
